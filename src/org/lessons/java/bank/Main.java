ebf876ca077da8f090fd3748777d988005431cac
LucaDalRi <euro2xd@gmail.com>
2023-06-06 14:11:44 +0200

    Inizializzazione progetto

--- src/org/lessons/java/bank/Main.java
+++ src/org/lessons/java/bank/Main.java
@@ -1,64 +1,17 @@
 package org.lessons.java.bank;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Conto myConto = new Conto("Luca");
+        Prodotto newProd = new Prodotto("Riso", "Riso roma", 0.85, 3);
 
-        Scanner s1 = new Scanner(System.in);
+        System.out.println("Codice Seriale: " + newProd.getSerialCode());
+        System.out.println("Nome prodotto: " + newProd.getName());
+        System.out.println("Nome completo prodotto " + newProd.getFullName());
+        System.out.println("Descrizione prodotto: " + newProd.getDescription());
+        System.out.println("Prezzo: " + newProd.getPrice());
+        System.out.println("Prezzo Tassato : " + newProd.taxedPrice());
+        System.out.println("Tasse: " + newProd.getTaxes() + "%");
 
-        boolean flag = true;
-
-        do {
-
-            System.out.println("Benvenuto! Premi 1 per depositare dei soldi, premi 2 per ritirare dei soldi, premi qualsiasi altro numero per uscire.");
-
-            int choice = s1.nextInt();
-
-            if (choice == 1) {
-
-                System.out.println("Quanto vuoi depositare?");
-
-                int depositAmount = s1.nextInt();
-
-                myConto.depositCash(depositAmount);
-
-                System.out.println("Deposito completato.");
-
-                System.out.println("Saldo corrente: " + myConto.getSaldoConto());
-
-            } else if (choice == 2) {
-
-                System.out.println("Quanto vuoi ritirare?");
-
-                int withdrawAmount = s1.nextInt();
-
-
-                if (myConto.getSaldoConto() > withdrawAmount) {
-
-                    myConto.withdrawCash(withdrawAmount);
-
-                    System.out.println("Ritiro completato.");
-
-                    System.out.println("Saldo corrente: " + myConto.getSaldoConto());
-
-                } else {
-
-                    System.out.println("Saldo insufficente!");
-
-                    System.out.println("Saldo corrente: " + myConto.getSaldoConto());
-                }
-
-
-            } else {
-
-                System.out.println("Arrivederci!");
-
-                flag = false;
-            }
-        }
-        while (flag);
     }
 }
